c48d9ebc81800533bfd273d9f97809c5a28f6123
wellcomez <zhu.jialai@gmail.com>
2024-07-08 20:33:03 +0800

    add

--- lspcpp.py
+++ lspcpp.py
@@ -2,6 +2,7 @@ import subprocess
 import pylspclient
 import threading
 from os import path
+from pylspclient.lsp_pydantic_strcuts import TextDocumentIdentifier, TextDocumentItem, LanguageIdentifier, Position, Range, CompletionTriggerKind, CompletionContext
 
 DEFAULT_CAPABILITIES = {
     'textDocument': {
@@ -69,8 +70,26 @@ class lspcppclient:
         if initialize_response['serverInfo']['name'] != 'clangd':
             raise RuntimeError("failed to initialize lsp_client")
         lsp_client.initialized()
-        self.client = lsp_client
+        self.lsp_client = lsp_client
         pass
+
+    def close(self):
+        self.lsp_client.exit()
+
+    def get_symbol(self, file):
+        uri = to_file(file)
+        relative_file_path = path.join(DEFAULT_ROOT, file)
+        uri = to_file(relative_file_path)
+        text = open(relative_file_path, "r").read()
+        version = 1
+        self.lsp_client.didOpen(
+            TextDocumentItem(uri=uri,
+                             languageId=LanguageIdentifier.CPP,
+                             version=version,
+                             text=text))
+        symbols = self.lsp_client.documentSymbol(
+            TextDocumentIdentifier(uri=uri))
+        return symbols
 
 
 class lspcppserver:
@@ -86,7 +105,7 @@ class lspcppserver:
         read_pipe.start()
         self.json_rpc_endpoint = pylspclient.JsonRpcEndpoint(p.stdin, p.stdout)
 
-    def newclient(self, confg: project_config):
+    def newclient(self, confg: project_config) -> lspcppclient:
         return lspcppclient(confg, self.json_rpc_endpoint)
 
 
@@ -101,3 +120,18 @@ class lspcpp:
         config.DEFAULT_ROOT = DEFAULT_ROOT
         self.client = self.serv.newclient(config)
         pass
+
+
+DEFAULT_ROOT = "/home/z/dev/lsp/pylspclient/tests/cpp/test-workspace"
+if __name__ == "__main__":
+    srv = lspcppserver()
+    cfg = project_config()
+    cfg.DEFAULT_ROOT = DEFAULT_ROOT
+    cfg.data_path = "/home/z/dev/lsp/pylspclient/tests/cpp/compile_commands.json"
+    client = srv.newclient(cfg)
+    file = "/home/z/dev/lsp/pylspclient/tests/cpp/test_main.cpp"
+    ss = client.get_symbol(file)
+    assert (len(ss) > 0)
+    for i in ss:
+        print(i.name, i.kind)
+    client.close()
--- tests/test_cpp.py
+++ tests/test_cpp.py
@@ -11,3 +11,16 @@ def test_client_init():
     cfg.data_path = "/home/z/dev/lsp/pylspclient/tests/cpp/compile_commands.json"
     client = lspcppserver.newclient(cfg)
     assert (client != None)
+
+
+def test_client_init():
+    lspcppserver = lspcpp.lspcppserver()
+    cfg = lspcpp.project_config()
+    cfg.DEFAULT_ROOT = DEFAULT_ROOT
+    cfg.data_path = "/home/z/dev/lsp/pylspclient/tests/cpp/compile_commands.json"
+    client = lspcppserver.newclient(cfg)
+    file = "/home/z/dev/lsp/pylspclient/tests/cpp/test_main.cpp"
+    ss = client.get_symbol(file)
+    assert (len(ss) > 0)
+    for i in ss:
+        print(i)

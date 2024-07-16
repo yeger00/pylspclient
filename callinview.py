12ec6b02ef3451881b1226bea6ad23575400b747
wellcomez <zhu.jialai@gmail.com>
2024-07-16 13:49:12 +0800

    add

--- callinview.py
+++ callinview.py
@@ -1,6 +1,7 @@
+from textual.validation import Failure
 from textual.widgets import Label, ListItem, ListView, Tree
 
-from lspcpp import task_call_in
+from lspcpp import CallNode, task_call_in
 
 
 class uicallback:
@@ -25,11 +26,62 @@ class MyListView(ListView):
         self.mainui.on_select_list(self)
 
 
+CallTreeNodeExpand = 3
+CallTreeNodeFocus = 2
+CallTreeNodeCollapse = 1
+
+
+class CallTreeNode:
+    state: int
+
+    def __init__(self, callnode: CallNode, expanded: bool) -> None:
+        self.callnode = callnode
+        self.state = CallTreeNodeExpand if expanded else CallTreeNodeCollapse
+        pass
+
+
+class _calltree(Tree):
+    BINDINGS = [
+        ("enter", "open_file", "Open"),
+    ]
+
+    def __init__(self):
+        Tree.__init__(self, "call heritage")
+
+    def action_toggle_node(self):
+        pass
+
+    def action_enter(self):
+        pass
+
+    def action_select_cursor(self):
+        if self.cursor_node != None and self.cursor_node.data!=None:
+            n:CallTreeNode = self.cursor_node.data
+            if self.cursor_node.is_expanded :
+                if n.state != CallTreeNodeExpand:
+                    n.state = CallTreeNodeExpand
+                    return
+            else:
+                if n.state != CallTreeNodeCollapse:
+                    n.state = CallTreeNodeCollapse
+                    return
+
+            if n.state==CallTreeNodeCollapse:
+                n.state = CallTreeNodeFocus
+                return
+            elif n.state==CallTreeNodeFocus:
+                n.state = CallTreeNodeExpand
+            else :
+                n.state = CallTreeNodeCollapse
+            self.cursor_node.toggle_all()
+        pass
+
+
 class callinview:
     job: task_call_in
 
     def __init__(self) -> None:
-        self.tree = Tree("call heritage")
+        self.tree = _calltree()
 
     # mainui:uicallback
     def update_job(self, job: task_call_in):
@@ -40,8 +92,16 @@ class callinview:
                                   expand=True,
                                   data=job.method)
         for a in job.callin_all:
-            node = root.add(a.displayname(), data=a, expand=True)
+            node = root.add(a.displayname(),
+                            data=CallTreeNode(a, True),
+                            expand=True)
             a = a.callee
             while a != None:
-                node = node.add(a.displayname(), data=a)
+                if a.callee is None:
+                    node = node.add_leaf(a.displayname(),
+                                         data=CallTreeNode(a, False))
+                    break
+                else:
+                    node = node.add(a.displayname(),
+                                    data=CallTreeNode(a, False))
                 a = a.callee
